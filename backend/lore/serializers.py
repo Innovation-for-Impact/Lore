6271ff0bb915bbb6d533627c108a0aebdbfaae01
aasmart <alexandersmart2005@gmail.com>
2025-03-02 22:56:39 -0500

    Note a todo item

--- backend/lore/serializers.py
+++ backend/lore/serializers.py
@@ -9,6 +9,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ["id", "first_name", "last_name"]
 
 
+# TODO: Fix serializer referencing said_by w/ url
 class QuoteSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Quote
