64b4448dd866329204c40b78d8cdd53f135e911b
aasmart <alexandersmart2005@gmail.com>
2025-03-08 14:17:40 -0500

    Google authentication and environment files

--- backend/lore/views/google_auth.py
+++ backend/lore/views/google_auth.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
 
+from api import settings
 
-class GoogleLogin(
-    SocialLoginView
-):  # if you want to use Implicit Grant, use this
+
+class GoogleLogin(SocialLoginView):
+    """Handles google AUTH.
+
+    Create a post request with the authentication code
+    provide by Google on the callback url.
+    """
+
     adapter_class = GoogleOAuth2Adapter
+    callback_url = settings.GOOGLE_AUTH_REDIRECT_URL
+    client_class = OAuth2Client
