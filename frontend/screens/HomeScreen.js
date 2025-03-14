997f4d300ae766f575628d7960a91bb69fc188e4
Flora Deng <florad@umich.edu>
2025-03-14 13:21:36 -0400

    Made sure component is visible on the home screen

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import SearchGroupBar from '../components/SearchGroup';
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Welcome to the Home Screen!</Text>
+      <SearchGroupBar />
     </View>
   );
 };
