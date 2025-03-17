f273c6e84adce45eef03567b79296fe602d07231
Flora Deng <florad@umich.edu>
2025-03-17 02:03:07 -0400

    working on styling first registration screen

--- frontend/App.js
+++ frontend/App.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native'; 
 import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import Navigation from './components/Navigation';
+import RegistrationScreen from "./screens/RegistrationScreen";
 
 
 const Stack = createStackNavigator(); 
 
 export default function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // check for a stored user using AsyncStorage
+  useEffect(() => {
+    const checkUser = async () => {
+      const storedUser = await AsyncStorage.getItem("user");
+      if (storedUser) {
+        setUser(JSON.parse(storedUser));
+      }
+      setLoading(false);
+    };
+    checkUser();
+  }, []); // runs useEffect once on component's first render
+
+  if (loading) return null;
+
   return (
     <SafeAreaProvider>
       <NavigationContainer> 
-        <Navigation />
+        <Stack.Navigator>
+          {!user ? (
+            <Stack.Screen name="Registration" component={RegistrationScreen} options={{ headerShown: false }} />
+          ) : (
+            <Stack.Screen name="Navigation" component={Navigation} options={{ headerShown: false }} 
+        />
+          )}
+        </Stack.Navigator>
       </NavigationContainer>
-    </SafeAreaProvider>
+  </SafeAreaProvider>
   );
 }
---
+++ frontend/screens/RegistrationScreen.js
@@ -0,0 +1,64 @@
+import React, { useContext, useEffect } from "react";
+import { Dimensions, View, Text, Button, StyleSheet, Image, TouchableOpacity } from "react-native";
+import Logo from '../assets/logo-transparent-white.png';
+
+const screenWidth = Dimensions.get('window').width;
+
+const RegistrationScreen = ({ navigation }) => {
+
+    return (
+        <View style={styles.container}>
+        <Text style={styles.title}>What's Your</Text>
+        <Image source={Logo} style={styles.img}/>
+        <Text style={styles.text}>Connect with friends, run up challenges, & do it for the plot</Text>
+        <TouchableOpacity style={styles.login} nPress={() => navigation.navigate("Login")}>
+            <Text style={styles.buttonText}>Log In</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.register} nPress={() => navigation.navigate("Register")}>
+            <Text style={styles.buttonText}>Create Account</Text>
+        </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: { 
+        backgroundColor: "#AFB0E4",
+        flex: 1, 
+        justifyContent: "center", 
+        alignItems: "center" 
+    },
+    title: { 
+        color: "#5F4078",
+        fontSize: 50, 
+        fontWeight: "bold", 
+        marginBottom: 20 
+    },
+    text: {
+        color: "#007FBC",
+        fontSize: 22, 
+        fontWeight: "400",
+        marginLeft: 45,
+        marginRight: 45,
+        textAlign: 'center',
+    },
+    img: {
+        width: screenWidth * 0.6,
+        height: screenWidth * 0.25,
+        marginBottom: 25,
+    },
+    login: {
+        backgroundColor: "#5F4078",
+        marginTop: 150,
+        padding: 10,
+    },
+    register: {
+        backgroundColor: "#9680B6",
+    },
+    buttonText: {
+        color: "#FFFF",
+        fontSize: 22,
+    }
+});
+
+export default RegistrationScreen;
