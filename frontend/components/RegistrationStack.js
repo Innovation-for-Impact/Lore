b03c57ef36a881e49ec7065015c71d8b9b739307
Arda Edil <arda.edil@hisarschool.k12.tr>
2025-03-27 15:59:53 -0400

    Add login screen updates

--- frontend/components/RegistrationStack.js
+++ frontend/components/RegistrationStack.js
@@ -2,6 +2,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import RegistrationScreen from '../screens/RegistrationScreen';
 import CreateAccountScreen from '../screens/CreateAccountScreen';
 import LoginScreen from '../screens/LoginScreen';
+import WelcomeBackScreen from '../screens/WelcomeBackScreen';
 
 const Stack = createStackNavigator();
 
@@ -21,6 +22,11 @@ function RegistrationStack() {
       <Stack.Screen 
         name="LoginScreen" 
         component={LoginScreen} 
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen 
+        name="WelcomeBack" 
+        component={WelcomeBackScreen}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
