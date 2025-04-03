c452d214ffad25ed3f29e2daa6020a5b920edd29
Flora Deng <florad@umich.edu>
2025-04-03 15:12:21 -0400

    working on community screen for quote board

---
+++ frontend/components/CommunityStack.js
@@ -0,0 +1,36 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import CommunityScreen from '../screens/CommunityScreen';
+import QuoteBoardScreen from '../screens/QuoteBoardScreen';
+import MemoryBoardScreen from '../screens/MemoryBoardScreen';
+import AchievementBoardScreen from '../screens/AchievementBoardScreen';
+
+const Stack = createStackNavigator();
+
+function CommunityStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="CommunityScreen"
+        component={CommunityScreen} 
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen 
+        name="QuoteBoardScreen"
+        component={QuoteBoardScreen} 
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen 
+        name="MemoryBoardScreen"
+        component={MemoryBoardScreen} 
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen 
+        name="AchievementBoardScreen"
+        component={AchievementBoardScreen} 
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default CommunityStack;
