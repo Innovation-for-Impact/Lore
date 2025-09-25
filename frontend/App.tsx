7426f507111783022ab75544eb2ea46568896887
ethansun <ethansun@umich.edu>
2025-09-25 19:28:39 -0400

    updated App.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,13 +1,12 @@
-import { React, useState, useEffect } from 'react';
-import { NavigationContainer } from '@react-navigation/native'; 
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useFonts } from 'expo-font';
+import { useState } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Navigation from './components/Navigation';
-import { useFonts } from 'expo-font';
 import RegistrationStack from './components/RegistrationStack';
-import WelcomeBackScreen from './screens/WelcomeBackScreen';
 
-const Stack = createStackNavigator(); 
+const Stack = createStackNavigator();
 
 export default function App() {
   const [user, setUser] = useState(false);
@@ -20,17 +19,21 @@ export default function App() {
 
   return (
     <SafeAreaProvider>
-      <NavigationContainer> 
+      <NavigationContainer>
         <Stack.Navigator>
           {!user ? (
             <Stack.Screen name="Registration" options={{ headerShown: false }}>
               {(props) => <RegistrationStack {...props} setUser={setUser} />}
             </Stack.Screen>
           ) : (
-            <Stack.Screen name="Navigation" component={Navigation} options={{ headerShown: false }}  />
+            <Stack.Screen
+              name="Navigation"
+              component={Navigation}
+              options={{ headerShown: false }}
+            />
           )}
         </Stack.Navigator>
       </NavigationContainer>
-  </SafeAreaProvider>
+    </SafeAreaProvider>
   );
 }
