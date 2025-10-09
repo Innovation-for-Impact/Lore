d50c6fab5e0090ef8067cb5744ccc2694c99c03f
MishtyBansal <mishtybansal3@gmail.com>
2025-10-09 19:15:10 -0400

    Mishty 10/9 challenge screen

--- frontend/screens/AchievementBoardScreen.tsx
+++ frontend/screens/AchievementBoardScreen.tsx
@@ -1,12 +1,13 @@
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 // const screenWidth = Dimensions.get('window').width;
 // const screenHeight = Dimensions.get('window').height;
 
 const AchievementBoardScreen = () => {
+  const [selected, setSelected] = useState(true);
   const navigation = useNavigation();
 
   const goBackToCommunity = () => {
@@ -21,7 +22,19 @@ const AchievementBoardScreen = () => {
       >
         <Ionicons name="arrow-back" size={35} color="white" />
       </TouchableOpacity>
-      <Text>This is the Achievement Board!</Text>
+      <View style = {styles.topBar}>
+        <TouchableOpacity style = {styles.topButton}>
+          <Text style = {styles.topButtonText}>challenges</Text>
+        </TouchableOpacity >
+        <TouchableOpacity style = {styles.topButton}>
+          <Text style = {styles.topButtonText} >achievements</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style = {styles.title}>achievements</Text>
+      <Text style = {styles.subtitle}>see all the badges you've earned & learn how you can earn more.</Text>
+      <TouchableOpacity style = {styles.buttonStyle}>
+        <Text style = {styles.buttonText}>create achievement</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -31,8 +44,52 @@ const styles = StyleSheet.create({
     backgroundColor: "#AFB0E4",
     flex: 1,
     justifyContent: "center",
+    // alignItems: "center",
+    paddingHorizontal: 20,
+
+  },
+  topBar: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  topButtonText: {
+    fontFamily: "Work Sans",
+    color: "#5F4078"
+  },
+  topButton: {
+    backgroundColor: "#F7EEFF",
+    borderRadius: 15,
+    padding: 5,
+    flex: 1,
+    alignItems: "center",
+    marginHorizontal: 10,
+    borderWidth: 2,
+    borderColor: "#5F4078",
+  },
+  title: {
+    marginTop: 20,
+    fontSize: 25,
+    fontFamily: "Work Sans",
+    textAlign: "left"
+  },
+  subtitle: {
+    fontSize: 15,
+    fontFamily: "Work Sans",
+    marginTop: 10
+  },
+  buttonStyle: {
+    backgroundColor: "#5F4078",
+    padding: 15,
+    borderRadius: 10,
+    marginTop: 10,
     alignItems: "center"
   },
+  buttonText: {
+    fontFamily: "Work Sans",
+    color: "white",
+    fontSize: 20,
+
+  }
 });
 
 export default AchievementBoardScreen;
