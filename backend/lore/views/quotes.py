87512bd9c9763320f8231aa6b408deabc5144729
Alexander Smart <aasmart@Alexanders-MacBook-Pro.local>
2025-11-23 22:34:41 -0500

    quotes endpoint now handles permissions correctly

--- backend/lore/views/quotes.py
+++ backend/lore/views/quotes.py
@@ -10,10 +10,10 @@ from rest_framework.exceptions import ParseError
 
 from lore import serializers
 from lore.models import LoreGroup, LoreUser, Quote
-from lore.utils import GroupMemberItemPermission
+from lore.utils import GroupMemberItemPermission, GroupMemberRoutePermissions
 
 
-class QuoteViewSet(viewsets.ModelViewSet):
+class BaseQuoteViewSet(viewsets.ModelViewSet):
     """Viewset for quotes.
 
     Supports filtering by group_id and said_by_id, and also searching by text
@@ -38,20 +38,6 @@ class QuoteViewSet(viewsets.ModelViewSet):
     ]
     search_fields: ClassVar[list[str]] = ["text"]
 
-    def get_queryset(self):
-        """Get all the quotes for the user's groups or the route's group."""
-        user: LoreUser = cast(LoreUser, self.request.user)
-        queryset = Quote.quotes
-        if self.kwargs.get("loregroup_pk") is not None:
-            queryset = queryset.filter(
-                group_id=self.kwargs["loregroup_pk"],
-            )
-        else:
-            user_groups = LoreGroup.groups.get_groups_with_user(user)
-            queryset = queryset.filter(group__in=user_groups)
-
-        return queryset.order_by("pk")
-
     def perform_create(self, serializer: serializers.QuoteSerializer) -> None:
         """Create the item in the database."""
         # Extract the group_id from query parameters
@@ -60,3 +46,44 @@ class QuoteViewSet(viewsets.ModelViewSet):
             msg = "Expected a group id"
             raise ParseError(msg)
         serializer.save(group=LoreGroup.groups.filter(pk=group_id).first())
+
+
+class AllUserGroupsQuoteViewSet(BaseQuoteViewSet):
+    """Displays the quotes for all groups the user is in."""
+
+    permission_classes: ClassVar[list[type[permissions.BasePermission]]] = [
+        IsAuthenticated,
+        GroupMemberItemPermission,
+    ]
+
+    def get_queryset(self):
+        """Get all the quotes for the user's groups or the route's group."""
+        user: LoreUser = cast("LoreUser", self.request.user)
+        queryset = Quote.quotes
+        user_groups = LoreGroup.groups.get_groups_with_user(user)
+        queryset = queryset.filter(group__in=user_groups)
+
+        return queryset.order_by("pk")
+
+
+class GroupQuoteViewSet(BaseQuoteViewSet):
+    """Displays the quotes for a specific group."""
+
+    permission_classes: ClassVar[list[type[permissions.BasePermission]]] = [
+        IsAuthenticated,
+        GroupMemberItemPermission,
+        GroupMemberRoutePermissions,
+    ]
+
+    def get_queryset(self):
+        """Get all the quotes for the user's groups or the route's group."""
+        user: LoreUser = cast("LoreUser", self.request.user)
+        queryset = Quote.quotes
+        if self.kwargs.get("loregroup_pk") is None:
+            return None
+
+        queryset = queryset.filter(
+            group_id=self.kwargs["loregroup_pk"],
+        )
+
+        return queryset.order_by("pk")
